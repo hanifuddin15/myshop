60a43dad7536268f97c63a6dd8862b338c2ef003
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-09-20 14:02:45 +0600

    custom appbar added

---
+++ lib/widgets/appbar/custom_appbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showBackButton;
+  final VoidCallback? onBackPressed;
+  final double fontSize;
+  final Color? titleColor;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.showBackButton = true,
+    this.onBackPressed,
+    this.fontSize = 30,
+    this.titleColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: fontSize,
+          color: titleColor ?? Colors.deepPurpleAccent,
+        ),
+      ),
+      centerTitle: true,
+      leading: showBackButton
+          ? IconButton(
+              onPressed: onBackPressed ?? () => context.pop(),
+              icon: const Icon(Icons.arrow_back_ios),
+            )
+          : null,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
