7f03fa789cbfbb927abb01ecfa59304183daf365
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-09-20 13:33:43 +0600

    router um=nastance and consumer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_shop/router/app_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
-      overrides: [
-        sharedPreferencesProvider.overrideWithValue(prefs),
-      ],
-      child: MaterialApp.router(
-        title: 'My Shop',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        ),
-        routerConfig: router,
+      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      child: Consumer(
+        builder: (context, ref, child) {
+          final routerInstance = ref.watch(routerProvider);
+          return MaterialApp.router(
+            title: 'My Shop',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurpleAccent,
+                primary: Colors.deepPurpleAccent,
+              ),
+            ),
+            routerConfig: routerInstance,
+          );
+        },
       ),
     ),
   );
