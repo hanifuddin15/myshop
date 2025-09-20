6907d0069961b782d2ee5f88b84346f7b639ffcf
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-09-20 14:04:39 +0600

    cart shimmer added

---
+++ lib/widgets/shimmer/cart_shimmer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerCartItemList extends StatelessWidget {
+  final int itemCount;
+
+  const ShimmerCartItemList({super.key, this.itemCount = 4});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: itemCount,
+      separatorBuilder: (_, _) => const SizedBox(height: 12),
+      itemBuilder: (context, index) => Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+    );
+  }
+}
