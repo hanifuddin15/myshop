ace6359d8d2cc78e3137ccd6786b65168ab7d399
Hanif Uddin <hanif.inno@pakizasoftware.com>
2025-09-20 13:35:38 +0600

    null saftey and some property added

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,25 +1,37 @@
 class Product {
   final String id;
   final String name;
-  final double price;
+  final double? prevPrice;
+  final double presentPrice;
   final String imageUrl;
   final String description;
+  final String? review;
+  final double? rating;
+  final String? details;
 
   Product({
     required this.id,
     required this.name,
-    required this.price,
+    this.prevPrice,
+    required this.presentPrice,
     required this.imageUrl,
     required this.description,
+    this.review,
+    this.rating,
+    this.details,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: json['id'] as String,
       name: json['name'] as String,
-      price: (json['price'] as num).toDouble(),
-      imageUrl: json['imageUrl'] as String,
-      description: json['description'] as String,
+      prevPrice: (json['prevPrice'] as num?)?.toDouble(),
+      presentPrice: (json['presentPrice'] as num?)?.toDouble() ?? 0.0,
+      imageUrl: json['imageUrl'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+      review: json['review'] as String?,
+      rating: (json['rating'] as num?)?.toDouble(),
+      details: json['details'] as String?,
     );
   }
 
@@ -27,9 +39,13 @@ class Product {
     return {
       'id': id,
       'name': name,
-      'price': price,
+      'prevPrice': prevPrice,
+      'presentPrice': presentPrice,
       'imageUrl': imageUrl,
       'description': description,
+      'review': review,
+      'rating': rating,
+      'details': details,
     };
   }
 }
